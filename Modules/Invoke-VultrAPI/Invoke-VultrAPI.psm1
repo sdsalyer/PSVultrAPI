0f22cc7e393984db0e4e7c2dd97057eb1533ca39
drew <spencer.salyer@gmail.com>
2017-10-25 10:58:58 -0400

    initial commit

---
+++ Modules/Invoke-VultrAPI/Invoke-VultrAPI.psm1
@@ -0,0 +1,100 @@
+#Requires -Version 3.0
+<#
+.SYNOPSIS
+Invoke the Vultr API
+
+.DESCRIPTION
+.PARAMETER <param1 name>
+.PARAMETER <param(n) name>
+.INPUTS
+.OUTPUTS
+.EXAMPLE
+.EXAMPLE
+.LINK
+#>
+function Invoke-VultrAPI {
+	[CmdletBinding()]
+	[OutputType([psobject])] # http response object ?
+	param()
+	begin {}
+	process{
+		#TODO: input parameters -- should apiKey be secured?
+		$apiUrl = 'https://api.vultr.com';
+		$apiKey = ''; #TODO: load from [encrypted] file?
+
+		# Configure the headers
+		$headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]";
+		$headers.Add("API-Key", $apiKey)
+
+		# Parameterless GET requests
+		#$apiCall = '/v1/account/info'; 
+		#$apiCall = '/v1/dns/list';
+		#$apiCall = '/v1/dns/records?domain=salyercreative.com';
+		#$parameters = '';
+		#$method = 'Get'; 
+
+		##POST Requests
+		# $apiCall = '/v1/dns/update_record';
+		# <# 
+			# domain string Domain name to update record
+			# RECORDID integer ID of record to update (see /dns/records)
+			# name string (optional) Name (subdomain) of record
+			# data string (optional) Data for this record
+			# ttl integer (optional) TTL of this record
+			# priority integer (optional) (only required for MX and SRV) Priority of this record (omit the priority from the data)
+		##>
+		# $parameters = @{
+		   # domain = 'salyercreative.com';
+		   # RECORDID = '4900476';
+		   # data = $myIp;
+		# };
+		# $method = 'Post';
+
+		$uri = "$apiUrl$apiCall";
+
+		Write-Debug "Request:`n`tURI: $uri`n`tMethod: $method`n`tHeaders: $(Out-String -InputObject $headers)`n`tParameters: $(Out-String -InputObject $parameters)";
+
+		if($method -eq 'Get') {
+			#$response = Invoke-RestMethod -Method $method -Uri $uri -Header $headers;
+			Write-Debug "Invoke-RestMethod -Method $method -Uri $uri -Header $headers";
+		}
+		elseif ($method -eq 'Post')  {
+			#$response = Invoke-RestMethod -Method $method -Uri $uri -Header $headers -Body $parameters;
+			Write-Debug "Invoke-RestMethod -Method $method -Uri $uri -Header $headers -Body $parameters";
+		}
+		else {
+			# TODO: what to return here
+			#$response = 'Invalid request method';
+			Write-Debug "$response = 'Invalid request method'";
+		}
+		
+		$response;
+	}
+	end{}
+}
+
+<#
+.SYNOPSIS
+Gets the current WAN IP address.
+
+.DESCRIPTION
+Gets the current WAN IP address by querying the ifconfig.me website.
+
+.OUTPUTS
+The current IP address of the machine.
+
+.EXAMPLE
+Get-WANIP
+123.12.12.123
+#>
+function Get-WANIP {
+	[CmdletBinding()]
+	[OutputType([String])]
+	param()
+	begin {}
+	process{
+		$myIpJSON = (Invoke-WebRequest ifconfig.me/all.json).Content | ConvertFrom-Json;
+		$myIpJSON.ip_addr;
+	}
+	end{}
+}
---
+++ Sync-DDNS.ps1
@@ -0,0 +1,98 @@
+#Requires -Version 3.0
+Import-Module Invoke-VultrAPI
+<#
+.SYNOPSIS
+A DDNS update client for the Vultr API.
+
+.DESCRIPTION
+Gets the current WAN IP and calls the Vultr API to update the A record.
+
+.PARAMETER <param1 name>
+.PARAMETER <param(n) name>
+.INPUTS
+.OUTPUTS
+.EXAMPLE
+.EXAMPLE
+.LINK
+#>
+function Sync-DDNS {
+	[CmdletBinding()]
+	[OutputType([psobject])]
+	param()
+	begin {
+		Write-Output "START: ===========================$(Get-TimeStamp)" | Out-File $logFile -Append -Force;
+	}
+	process{
+		# IP Checking and logging setup
+		# TODO: Make these input params?
+		$logFile = 'enterprise_ddns.log';
+		$ipFile = 'last_ip.json';
+		$myIp = . Get-WANIP; # TODO: is the dot import necessary?
+		$myLastIp = '';
+		
+		# Read the json file and set the last known IP
+		if (Test-Path $ipFile) {
+			$myLastIpJSON = Get-Content -Raw $ipFile | ConvertFrom-Json;
+			$myLastIp = $myLastIpJSON.ip_addr
+		}
+
+		# Log the current and previous IP
+		Write-Output "$(Get-TimeStamp) Last IP: $myLastIp" | Out-File $logFile -Append -Force;
+		Write-Output "$(Get-TimeStamp) Curr IP: $myIp" | Out-File $logFile -Append -Force;
+
+		# Exit if no change.
+		if($myLastIp -eq $myIp) {
+			exit;
+		}
+		Write-Output @{ ip_addr = $myIp } | ConvertTo-JSON | Out-File $ipFile -Force
+
+		
+		# TODO: call the Vultr API here
+		# POST Requests
+		$apiCall = '/v1/dns/update_record';
+		<# 
+			domain string Domain name to update record
+			RECORDID integer ID of record to update (see /dns/records)
+			name string (optional) Name (subdomain) of record
+			data string (optional) Data for this record
+			ttl integer (optional) TTL of this record
+			priority integer (optional) (only required for MX and SRV) Priority of this record (omit the priority from the data)
+		#>
+
+		# RECORDID in GET /v1/dns/records?domain=salyercreative.com
+		$parameters = @{
+		   domain = 'salyercreative.com';
+		   RECORDID = '4900476';
+		   data = $myIp;
+		};
+		$method = 'Post';
+		$response = Invoke-VultrAPI -Method $method -Call $apiCall -Parameters $parameters
+
+		Write-Output "$(Get-TimeStamp) Response:`n`t$(Out-String -InputObject $response)" | Out-File $logFile -Append -Force;
+
+	}
+	end{
+		Write-Output "END: =============================$(Get-TimeStamp)" | Out-File $logFile -Append -Force;
+	}
+}
+
+<#
+.SYNOPSIS
+Gets a formatted timestamp for logging.
+
+.DESCRIPTION
+Returns a formatted string with the date in MM/dd/yy formatted
+and the time in HH:mm:ss format, created from the Get-Date cmdlet.
+
+.EXAMPLE
+Get-TimeStamp
+[10/20/17 17:53:45]
+
+Write-Output "$(Get-TimeStamp) Log this message" | Out-File $myLogFile -Append
+This would write:
+[10/20/17 17:53:45]  Log this message
+to $myLogFile
+#>
+function script:Get-TimeStamp {
+    "[{0:MM/dd/yy} {0:HH:mm:ss}]" -f (Get-Date);
+}
